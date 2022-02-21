7bd805faa9043b4cf7340e272c9f808034aa5850
Asude Şenözlüler <senozlulerasude@gmail.com>
2022-02-21 16:32:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        ListWorker worker =  new ListWorker();
+        
+        
+        worker.calistir();
+        
+       
+       
+       
+      
+    }
+    
+}
